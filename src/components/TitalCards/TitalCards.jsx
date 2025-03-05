38babd929debf9b56f4a65fd3a59257829b8c9b1
Arunprakash Pm <arunprakashappm@gmail.com>
2025-03-05 09:46:56 +0300

    Popular on netflix image added

--- src/components/TitalCards/TitalCards.jsx
+++ src/components/TitalCards/TitalCards.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import "./TitalCards"
+import "./TitalCards.css"
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitalCards = () => {
   return (
     <div class="titalcards">
-      
+      <h2>Popular on Netflix</h2>
+      <div className='card-list'>
+        {cards_data.map((card,index)=>{
+              return <div className="card" key={index}>
+                <img src={card.image} alt=""/>
+                <p>{card.name}</p>
+                </div>
+        })
+
+        }
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TitalCards from '../../components/TitalCards/TitalCards'
 
 const Home = () => {
   return (
@@ -19,10 +20,11 @@ const Home = () => {
             immortal enemy.</p>
             <div className="hero-btns">
               <button className='btn'><img src={play_icon} alt=""/>Play</button>
-              <button className='btn drak-btn'><img src={info_icon} alt=""/>Play</button>
+              <button className='btn drak-btn'><img src={info_icon} alt=""/>More Info</button>
             </div>
+            <TitalCards/>
         </div>
-      </div>      
+      </div> 
     </div>
   )
 }
