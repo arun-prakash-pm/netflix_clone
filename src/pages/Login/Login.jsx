4558d8c45f0df0d38d815664d296851db5932827
Arunprakash Pm <arunprakashappm@gmail.com>
2025-03-06 08:57:19 +0300

    Intial commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import "./Login.css"
+import logo from '../../assets/logo.png'
 
 function Login() {
   return (
     <div className="login">
-      
-    </div>
+      <img src={logo} className='login-logo' alt=""/>
+      <div className='login-form'>
+        <p>Sign In</p>
+        <form>
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign In</button>
+          <div className='form-help'>
+            <div className='remember'>
+              <input type='checkbox'/>
+                <label htmlFor=''>Remember Me</label>
+            </div>   
+            <p>Need Help?</p> 
+          </div>
+        </form>
+      </div>
+     </div>
   )
 }
 
